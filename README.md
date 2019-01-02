2662289f5b508126c270f15fcc96193e9e0d6111
aggresss <aggresss@163.com>
2019-01-02 14:41:21 +0800

    Add test and ci framework

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-Fork from http://ftp.gnome.org/pub/GNOME/sources/libghttp/1.0/libghttp-1.0.9.tar.gz
 
-Apply patch from repo https://github.com/pld-linux/lighttp.git
+# libghttp
+
+[![GitHub version](https://badge.fury.io/gh/aggresss%2Flibghttp.svg)](https://badge.fury.io/gh/aggresss%2Flibghttp)
+[![GitHub license](https://img.shields.io/github/license/aggresss/libghttp.svg)](https://github.com/aggresss/libghttp/blob/master/LICENSE)
+[![Build Status](https://www.travis-ci.org/aggresss/libghttp.svg?branch=master)](https://www.travis-ci.org/aggresss/libghttp)
+[![Coverage Status](https://coveralls.io/repos/github/aggresss/libghttp/badge.svg?branch=master)](https://coveralls.io/github/aggresss/libghttp?branch=master)
+
+---
+
+### Infrasturcture:
+
+- [x] Fork from http://ftp.gnome.org/pub/GNOME/sources/libghttp/1.0/libghttp-1.0.9.tar.gz
+- [x] Apply patch from repo https://github.com/pld-linux/lighttp.git
---
+++ test/test_http.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include "ghttp.h"
+#include "CuTest.h"
+
+void test_http_setup(CuTest *tc)
+{
+
+}
+
+
+void test_http_get(CuTest *tc)
+
+{
+    char *uri = "http://kodo.router7.com/index.html";
+    ghttp_request *request = NULL;
+    ghttp_status status;
+    char *buf;
+    int bytes_read;
+
+    request = ghttp_request_new();
+    if(ghttp_set_uri(request, uri) == -1)
+        exit(-1);
+    if(ghttp_set_type(request, ghttp_type_get) == -1)
+        exit(-1);
+    ghttp_prepare(request);
+    status = ghttp_process(request);
+    if(status == ghttp_error)
+        exit(-1);
+    /* OK, done */
+    CuAssertIntEquals(tc, 200, ghttp_status_code(request));
+    buf = ghttp_get_body(request);
+    bytes_read = ghttp_get_body_len(request);
+    return ;
+}
+
+
+void test_http_cleanup(CuTest *tc)
+{
+
+}
+
+
+
+CuSuite *test_http(CuTest *tc)
+{
+        CuSuite* suite = CuSuiteNew();
+
+        SUITE_ADD_TEST(suite, (void*)test_http_setup);
+        SUITE_ADD_TEST(suite, (void*)test_http);
+        SUITE_ADD_TEST(suite, (void*)test_http_cleanup);
+
+        return suite;
+}
