d764b8bb4371803f0e76b9f212a172213e4653f2
aggresss <aggresss@163.com>
2019-02-12 19:57:16 +0800

    2019-02-12 19:57:15 CST W06D2

--- src/http_uri.c
+++ src/http_uri.c
@@ -62,7 +62,7 @@ http_uri_parse(const char *a_string,
     
     /* change default port for secure connections */
     if(!strcmp(a_uri->proto, "https")) {
-#if defined (USE_OPENSSL) || (USE_WOLFSSL)
+#if defined(USE_OPENSSL) || defined(USE_WOLFSSL)
       a_uri->port = 443; 
 #else
       goto ec;
