cfb6985096430191d5f9a7a8b5717a48b1498387
aggresss <aggresss@163.com>
2019-03-23 17:50:09 +0800

    2019-03-23 17:50:09 CST W11D6

--- tests/test_ghttp.c
+++ tests/test_ghttp.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #include "flag/flag.h"
 #include "log/log.h"
@@ -11,7 +12,6 @@
 #define GHTTP_TEST_FAIL (-1)
 
 int test_http_get(void)
-
 {
     int ret = GHTTP_TEST_FAIL;
     char *uri = "http://cn.bing.com/favicon.ico";
@@ -48,8 +48,82 @@ ec:
 }
 
 
+int test_http_download()
+{
+    int ret = GHTTP_TEST_FAIL;
+    char *uri = "http://link.router7.com:8091/Cp8ugVw-i8WAJU9EAEnLHjlJfMc620.zip";
+    ghttp_request *request = NULL;
+    ghttp_status status;
+    ghttp_current_status current_status;
+    int http_code;
+    char *buf;
+    int nBodyLen = 0;
+    int nTotalLen = 0;
+    FILE* fptr;
+    fptr = fopen("/tmp/http_down.test", "wb");
+    if (fptr == NULL) {
+        LogError("Error to open file!");
+        goto ec;
+    }
+
+    /* create new request */
+    request = ghttp_request_new();
+
+    /* set request*/
+    if(ghttp_set_uri(request, uri) == -1)
+        goto ec;
+    if(ghttp_set_type(request, ghttp_type_get) == -1)
+        goto ec;
+    if(ghttp_set_sync(request, ghttp_async) == -1)
+        goto ec;
+    ghttp_set_timeout(request, 5);
+    ghttp_set_chunksize(request, 256*1024);
+
+    /* connect */
+    ghttp_prepare(request);
+
+    /* receive */
+    do {
+        status = ghttp_process(request);
+        current_status = ghttp_get_status(request);
+        LogDebug("ghttp current status: %d, read: %d, total:%d",
+                current_status.proc, current_status.bytes_read, current_status.bytes_total);
+        switch (current_status.proc) {
+        case ghttp_proc_request:
+            LogDebug("Send http request.");
+            break;
+        case ghttp_proc_response_hdrs:
+            LogDebug("Process header.");
+            break;
+        case ghttp_proc_response:
+            LogDebug("Downloads.");
+            break;
+        default:
+            break;
+        }
+
+    } while (status != ghttp_done);
+
+    buf = ghttp_get_body(request);
+     if (buf) {
+         nBodyLen = ghttp_get_body_len(request);
+         ret = fwrite(buf, 1, nBodyLen, fptr);
+         if (ret != nBodyLen)
+             goto ec;
+     }
+
+
+    ret = GHTTP_TEST_SUCCESS;
+ec:
+    ghttp_request_destroy(request);
+    fclose(fptr);
+    return ret;
+}
+
+
 typedef struct _CmdArg {
         bool bTestHttpGet;
+        bool bTestHttpDown;
 }CmdArg;
 
 int main(int argc, const char * argv[])
@@ -58,14 +132,27 @@ int main(int argc, const char * argv[])
     CmdArg cmdArg;
     memset(&cmdArg, 0, sizeof(cmdArg));
     cmdArg.bTestHttpGet = false;
+    cmdArg.bTestHttpDown = false;
 
     SetLogLevel(LOG_LEVEL_TRACE);
 
     flag_bool(&cmdArg.bTestHttpGet, "http_get", "test http get");
+    flag_bool(&cmdArg.bTestHttpDown, "http_download", "test http download");
 
     flag_parse(argc, argv, "test libghttp");
+
     if (cmdArg.bTestHttpGet) {
         ret = test_http_get();
+        if (ret != 0) {
+            goto ec;
+        }
+    }
+
+    if (cmdArg.bTestHttpDown) {
+        ret = test_http_download();
+        if (ret != 0) {
+            goto ec;
+        }
     }
 ec:
     return ret;
